22056ec5dd1e8278fc584fac19e4acb4381d7209
karthik h r <karthikhrd17@gmail.com>
2024-08-05 17:13:19 +0530

    first chane

--- server/server/utils/encryption.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-exports.hashPassword = async (password) => {
-  return bcrypt.hash(password, 12);
-};
-
-exports.comparePassword = async (password, hashedPassword) => {
-  return bcrypt.compare(password, hashedPassword);
-};
